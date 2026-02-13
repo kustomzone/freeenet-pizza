a82ffe0922308b46be6565505554159ac405788f
Maciej Krüger <mkg20001@gmail.com>
2026-02-13 19:19:25 +0100

    feat: add web-container-sign

--- deploy-tool/src/main.rs
+++ deploy-tool/src/main.rs
@@ -6,9 +6,10 @@ use std::process::Command;
 
 pub const PROJECT: &str = "pizza-freenet";
 
-fn default_storage_path() -> PathBuf {
+fn default_storage_path(file: &str) -> PathBuf {
     let mut p = dirs::config_dir().expect("Could not find config directory");
     p.push(PROJECT);
+    p.push(file);
     p
 }
 
@@ -24,9 +25,13 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     /// Deploy the application
-    Deploy { },
+    Deploy {
+        /// Version number
+        #[arg(long, short, default_value = "1")]
+        version: u32,
+    },
     /// Launch application in development mode
-    Dev { },
+    Dev {},
 }
 
 fn cargo_build(package: &str) -> Result<(), Box<dyn Error>> {
@@ -42,13 +47,49 @@ fn cargo_build(package: &str) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+fn web_container_sign(
+    input: PathBuf,
+    output: PathBuf,
+    parameters: PathBuf,
+    version: u32,
+) -> Result<(), Box<dyn Error>> {
+    println!("Signing web container: {} -> {}", input.display(), output.display());
+    let status = Command::new("cargo")
+        .args([
+            "run",
+            "--bin",
+            "web-container-tool",
+            "--",
+            "sign",
+            "--input",
+            &input.to_string_lossy(),
+            "--output",
+            &output.to_string_lossy(),
+            "--parameters",
+            &parameters.to_string_lossy(),
+            "--version",
+            &version.to_string(),
+        ])
+        .status()?;
+
+    if !status.success() {
+        return Err("web-container-tool sign failed".into());
+    }
+
+    Ok(())
+}
+
 fn dev() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn deploy() -> Result<(), Box<dyn Error>> {
-    cargo_build("pizza-contract");
-    cargo_build("pizza-ui");
+fn deploy(version: u32) -> Result<(), Box<dyn Error>> {
+    cargo_build("pizza-contract")?;
+    cargo_build("pizza-ui")?;
+
+    let input = PathBuf::from("webapp-bootstrap.tar.xz");
+    let output = default_storage_path("webapp.metadata");
+    web_container_sign(input, output, default_storage_path("webapp.parameters"), version)?;
 
     Ok(())
 }
@@ -56,7 +97,7 @@ fn deploy() -> Result<(), Box<dyn Error>> {
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
     match cli.command {
-        Commands::Deploy { } => deploy(),
-        Commands::Dev { } => dev(),
+        Commands::Deploy { version } => deploy(version),
+        Commands::Dev {} => dev(),
     }
 }
