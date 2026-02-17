3556d08eaec47b3783388aa773fc5604c1afd444
Maciej Krüger <mkg20001@gmail.com>
2026-02-17 02:02:50 +0100

    fix: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,4 @@
 
 set -euxo pipefail
 
-if [ ! -f "webapp.parameters" ]; then
-    echo "bootstrap" > /tmp/webapp-bootstrap.tar.xz
-    cargo run --bin web-container-tool -- sign \
-        --input /tmp/webapp-bootstrap.tar.xz \
-        --output /tmp/webapp-bootstrap.metadata \
-        --parameters "webapp.parameters" \
-        --version 1 2>/dev/null
-    # rm -f /tmp/webapp-bootstrap.tar.xz /tmp/webapp-bootstrap.metadata
-fi
+nix develop -c cargo run --bin deploy-tool deploy
